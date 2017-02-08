aaf285879412ce72809b5124a931cf7efcbeff71
wodrichw <wodrichw@oregonstate.edu>
2017-02-07 16:56:04 -0800

    add files

---
+++ webglDemo.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("canvas");
+canvas.width = 300;
+canvas.height = 300;
+
+canvas.style.backgroundColor = "red";
+canvas.style.width = "100%";
+canvas.style.height = "100%";
